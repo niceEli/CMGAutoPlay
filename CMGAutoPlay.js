28de712457acf94d2ef0b1cb1919d612384ac58c
Eli <78963198+niceEli@users.noreply.github.com>
2023-05-11 11:39:53 -0400

    Update CMGAutoPlay.js

--- CMGAutoPlay.js
+++ CMGAutoPlay.js
@@ -1,51 +1,51 @@
 // ==UserScript==
 // @name         CoolMathGames Auto /play (Free Adless Fullscreen)
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  Automatically add /play to the end of the URL on CoolMathGames game pages removing the ads and making the game fullscreen.
+// @version      2.0.0
+// @description  Automatically add /play to the end of the URL on CoolMathGames game pages removing the ads and making the game fullscreen, excluding specified URLs.
 // @author       niceEli
 // @match        https://*.coolmathgames.com/*
 // @grant        none
-// @licence      MIT
+// @license      MIT
 // ==/UserScript==
 
-(function(){
-  // Check if the current URL should be excluded from redirection
-  function isExcludedUrl(url, excludedUrls) {
-    for (var i = 0; i < excludedUrls.length; i++) {
-      if (url.indexOf(excludedUrls[i]) > -1) {
-        return true;
-      }
+(function() {
+  'use strict';
+
+  async function getExcludedUrls() {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://niceeli.github.io/cmgExclude.txt');
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var excludedUrls = xhr.responseText.trim().split('\n');
+          resolve(excludedUrls);
+        } else {
+          reject(xhr.status);
+        }
+      };
+      xhr.onerror = function() {
+        reject(xhr.status);
+      };
+      xhr.send();
+    });
+  }
+
+  async function redirectToPlayUrl() {
+    var excludedUrls = await getExcludedUrls();
+    if (
+      location.href.indexOf('coolmathgames.com') > -1 &&
+      location.href.indexOf('/0-') > -1 &&
+      location.href.indexOf('/play') == -1 &&
+      !isExcludedUrl(location.href, excludedUrls)
+    ) {
+      location.href += '/play';
     }
-    return false;
   }
 
-  // Retrieve the list of excluded URLs from a remote TXT file
-  function getExcludedUrls(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://niceeli.github.io/cmgExclude.txt'); // Replace with your remote TXT file URL
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var excludedUrls = xhr.responseText.trim().split('\n');
-        callback(excludedUrls);
-      }
-    };
-    xhr.send();
-  }
-
-  // Redirect to the "/play" URL if applicable
-  function redirectToPlayUrl() {
-    var excludedUrls = [];
-    getExcludedUrls(function(result) {
-      excludedUrls = result;
-      if (
-        location.href.indexOf('coolmathgames.com') > -1 &&
-        location.href.indexOf('/0-') > -1 &&
-        location.href.indexOf('/play') == -1 &&
-        !isExcludedUrl(location.href, excludedUrls)
-      ) {
-        location.href += '/play';
-      }
+  function isExcludedUrl(url, excludedUrls) {
+    return excludedUrls.some(function(excludedUrl) {
+      return url.indexOf(excludedUrl) > -1;
     });
   }
 
